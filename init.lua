cf6b8bce9e709413bea2b1d3d03dda5e4d357d0f
dongjinhai <1650152108@qq.com>
2023-03-19 23:09:58 +0800

    适配lazy.nvim包管理器，适配vscode操作

---
+++ init.lua
@@ -0,0 +1,48 @@
+-- Map leader to space
+vim.g.mapleader = ' '
+
+
+-- Common settings
+vim.o.nohlsearch = true
+vim.o.incsearch = true
+vim.o.smartcase = true
+vim.o.showmode = true
+vim.o.scrolloff = 5
+vim.o.number = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = 4
+vim.o.smartindent = true
+vim.o.cursorline = true
+vim.o.clipboard = 'unnamedplus'
+
+
+-- Key mappings -- 
+vim.keymap.set('', 'Q', 'gq')
+vim.keymap.set({'n', 'v'}, 'H', '^', { noremap = true })
+vim.keymap.set({'n', 'v'}, 'L', '$', { noremap = true })
+vim.keymap.set({'n', 'v'}, 'J', '5<Down>', { noremap = true })
+vim.keymap.set({'n', 'v'}, 'K', '5<Up>', { noremap = true })
+
+vim.keymap.set('n', '<Leader>sc', ':nohlsearch<CR>', { noremap = true })
+vim.keymap.set('n', '<Leader>fs', ':Write<CR>', { noremap = true })
+vim.keymap.set('n', '<Leader>J', 'J', { noremap = true })
+
+
+-- Plugins manager --
+-- require("config.lazy")
+
+
+if not vim.g.vscode then
+    -- Auto command
+    -- 自动将光标移动到上次关闭文件前的位置
+    vim.api.nvim_create_autocmd("BufReadPost", {
+        pattern = {"*"},
+        callback = function()
+            if vim.fn.line("'\"") > 1 and vim.fn.line("'\"") <= vim.fn.line("$") then
+                vim.api.nvim_exec("normal! g'\"",false)
+            end
+        end
+    })
+end
+
---
+++ lua/config/lazy.lua
@@ -0,0 +1,35 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    { "folke/which-key.nvim", lazy = true },
+    { 
+        "numToStr/Comment.nvim",
+        init = function()
+            require("Comment").setup()
+        end
+    },
+    {
+        "kylechui/nvim-surround",
+        init = function()
+            require("nvim-surround").setup({})
+        end
+    },
+    { 
+        'echasnovski/mini.ai',
+        version = '*',
+        init = function()
+            require("mini.ai").setup()
+        end
+    },
+})
