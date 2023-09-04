22cfdadddfc4a08824041e9581cbaed4107bc3c9
dongjinhai <1650152108@qq.com>
2023-09-05 00:01:49 +0800

    vscode中使用vsiual和select模式注释后自动执行esc命令

--- lua/config/vscode.lua
+++ lua/config/vscode.lua
@@ -1,13 +1,14 @@
 --- 使用vscode自带的注释功能
 vim.keymap.set('n', 'gcc',
     function()
-        require("vscode-neovim").call("editor.action.commentLine")
+        require("vscode-neovim").call("editor.action.commentLine") 
     end,
     { noremap = true }
 )
 vim.keymap.set({'x', 'n', 'o'}, 'gc',
     function()
-        require("vscode-neovim").call("editor.action.commentLine")
+        require("vscode-neovim").call("editor.action.commentLine") 
+        vim.api.nvim_input('<Esc>')
     end,
     { noremap = true }
 )
