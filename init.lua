cf849c4cb382d47196990a50c97c2a9dce133bda
dongjinhai <1650152108@qq.com>
2024-03-22 08:20:25 +0800

    完善配置

--- init.lua
+++ init.lua
@@ -1,19 +1,26 @@
 -- Map leader to space
 vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 -- options setting --
-vim.o.nohlsearch = true
-vim.o.incsearch = true
-vim.o.smartcase = true
-vim.o.showmode = true
-vim.o.scrolloff = 5
-vim.o.number = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.showmode = true
+vim.opt.scrolloff = 5
+vim.opt.number = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = 4
 vim.o.smartindent = true
 vim.o.cursorline = true
 vim.o.clipboard = 'unnamedplus'
+
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- keymaps setting --
 require("keymaps")
